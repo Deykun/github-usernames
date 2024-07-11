db4ffa6175628eee01a184e299ffb9cbde53e561
Szymon Tondowski <stdeykun+git@gmail.com>
2024-07-12 00:47:13 +0200

    Build process added to repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# github-usernames-to-names
+# Usernames to names - GitHub
+
+Replace the username with the actual name of the user from their profile. This is useful if you work in an organization where usernames are ambiguous.
---
+++ src/template.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @namespace       deykun
+// @name            Usernames to names - GitHub
+// @name:en         Usernames to names - GitHub
+// @description     Replace the username with the actual name of the user from their profile. This is useful if you work in an organization where usernames are ambiguous.
+// @description:en  Replace the username with the actual name of the user from their profile. This is useful if you work in an organization where usernames are ambiguous.
+// @author          deykun
+// @version         0.9
+// @include	        https://*github.com*
+// @grant           none
+// @run-at		      document-start
+// @updateURL       https://raw.githubusercontent.com/Deykun/github-usernames-to-names/main/github-usernames-to-names.user-srcipt.js
+// @downloadURL     https://raw.githubusercontent.com/Deykun/github-usernames-to-names/main/github-usernames-to-names.user-srcipt.js
+// ==/UserScript==
+
+'use strict';
+
+window.U2N = {
+  version: 0.9,
+  isDevMode: true,
+}
+
+const userScriptLogger = (params) => {
+    if (params.isError) {
+        const { isCritical = false, message = '', error } = params;
+
+        if (isCritical) {
+            console.error(`A User2Names error (from Tampermonkey) has occurred. You can ignore it, or describe the error and create an issue here: https://github.com/Deykun/github-usernames-to-names/issues`);
+            console.error(`U2N error: ${message}`);
+            console.error(error);
+        }
+
+        if (window.U2N.isDevMode && error) {
+            console.error(error);
+        }
+    }
+};
+
+const domReady = (fn) => {
+    document.addEventListener('DOMContentLoaded', fn);
+    if (document.readyState === 'interactive' || document.readyState === 'complete') {
+        fn();
+    }
+};
+
+const initU2N = async () => {
+    try {
+      console.log('Hi.')
+
+    } catch (error) {
+        userScriptLogger({
+            isError: true, isCritical: true, message: 'initU2N() failed', error,
+        });
+
+        throw error;
+    }
+};
+
+domReady(initU2N);
