e83f641c091b67f9424a30b2dd01bbf03aa70203
Szymon Tondowski <stdeykun+git@gmail.com>
2024-07-14 20:18:25 +0200

    Updated developer script version that is easier to update.

--- public/github-usernames-to-names.user-srcipt.js
+++ public/github-usernames-to-names.user-srcipt.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @namespace       deykun
 // @name            Usernames to names - GitHub
-// @description     Replace the username with the actual name of the user from their profile. This is useful if you work in an organization where usernames are ambiguous.
+// @description     Replace ambiguous usernames with actual names from user profiles.
 // @author          deykun
 // @version         0.9
-// @include	        https://*github.com*
+// @include         https://*github.com*
 // @grant           none
-// @run-at		    document-start
+// @run-at          document-start
 // @updateURL       https://raw.githubusercontent.com/Deykun/github-usernames-to-names/main/github-usernames-to-names.user-srcipt.js
 // @downloadURL     https://raw.githubusercontent.com/Deykun/github-usernames-to-names/main/github-usernames-to-names.user-srcipt.js
 // ==/UserScript==
@@ -16,105 +16,113 @@
 window.U2N = {
   version: 0.9,
   isDevMode: true,
-  cache: { 
+  cache: {
     HTML: {},
     CSS: {},
   },
-}
+};
+
 
 const userScriptLogger = (params) => {
-    if (params.isError) {
-        const { isCritical = false, message = '', error } = params;
+  if (params.isError) {
+    const { isCritical = false, message = '', error } = params;
 
-        if (isCritical) {
-            console.error(`A User2Names error (from Tampermonkey) has occurred. You can ignore it, or describe the error and create an issue here: https://github.com/Deykun/github-usernames-to-names/issues`);
-            console.error(`U2N error: ${message}`);
-            console.error(error);
-        }
-
-        if (window.U2N.isDevMode && error) {
-            console.error(error);
-        }
+    if (isCritical) {
+      // eslint-disable-next-line no-console
+      console.error('A User2Names error (from Tampermonkey) has occurred. You can ignore it, or describe the error and create an issue here: https://github.com/Deykun/github-usernames-to-names/issues');
+      // eslint-disable-next-line no-console
+      console.error(`U2N error: ${message}`);
+      // eslint-disable-next-line no-console
+      console.error(error);
     }
+
+    if (window.U2N.isDevMode && error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }
 };
 
 const domReady = (fn) => {
-    document.addEventListener('DOMContentLoaded', fn);
-    if (document.readyState === 'interactive' || document.readyState === 'complete') {
-        fn();
-    }
+  document.addEventListener('DOMContentLoaded', fn);
+  if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    fn();
+  }
 };
 
 const initU2N = async () => {
-    try {
+  try {
     const appendCSS = (styles, { sourceName = '' } = {}) => {
   const appendOnceSelector = sourceName ? `g-u2n-css-${sourceName}`.trim() : undefined;
   if (appendOnceSelector) {
-      /* Already appended */
-      if (document.getElementById(appendOnceSelector)) {
-          return;
-      }
+    /* Already appended */
+    if (document.getElementById(appendOnceSelector)) {
+      return;
+    }
   }
 
   const style = document.createElement('style');
   if (sourceName) {
-      style.setAttribute('id', appendOnceSelector);
+    style.setAttribute('id', appendOnceSelector);
   }
 
   style.innerHTML = styles;
   document.head.append(style);
 };
 
+// eslint-disable-next-line default-param-last
 const render = (HTML = '', source) => {
-    const id = `g-u2n-html-${source}`
+  const id = `g-u2n-html-${source}`;
 
-    if (HTML === window.U2N.cache.HTML[id]) {
-        /* Don't rerender if HTML is the same */
-        return;
-    }
+  if (HTML === window.U2N.cache.HTML[id]) {
+    /* Don't rerender if HTML is the same */
+    return;
+  }
 
-    window.U2N.cache.HTML[id] = HTML;
+  window.U2N.cache.HTML[id] = HTML;
 
-    const wrapperEl = document.getElementById(id);
+  const wrapperEl = document.getElementById(id);
 
-    if (!HTML) {
-        wrapperEl?.remove();
-
-        return;
-    }
-
+  if (!HTML) {
     if (wrapperEl) {
-        wrapperEl.innerHTML = HTML;
-
-        return;
+      wrapperEl.remove();
     }
 
-    const el = document.createElement('div');
-    el.id = id;
-    el.setAttribute('data-testid', id);
-    el.innerHTML = HTML;
+    return;
+  }
 
-    document.body.appendChild(el);
+  if (wrapperEl) {
+    wrapperEl.innerHTML = HTML;
+
+    return;
+  }
+
+  const el = document.createElement('div');
+  el.id = id;
+  el.setAttribute('data-testid', id);
+  el.innerHTML = HTML;
+
+  document.body.appendChild(el);
 };
-    
-const getUserElements = () => {
+
+    const getUserElements = () => {
   const links = Array.from(document.querySelectorAll('[data-hovercard-url^="/users/"]')).map((el) => {
-    const username = el.getAttribute('data-hovercard-url').match(/users\/([A-Za-z0-9_-]+)\//)[1]
+    const username = el.getAttribute('data-hovercard-url').match(/users\/([A-Za-z0-9_-]+)\//)[1];
 
     if (username && el.textContent.includes(username)) {
       return {
         el,
         username,
-      }
+      };
     }
 
     return undefined;
-  }).filter(Boolean)
+  }).filter(Boolean);
 
   return links;
-}
+};
 
-appendCSS(`
+appendCSS(` 
   [data-u2n-username]::before {
     display: inline-block;
     align-self: center;
@@ -141,24 +149,23 @@ appendCSS(`
     background: #dbedff !important;
   }
 
-`, { sourceName: 'render-users' })
+`, { sourceName: 'render-users' });
 
 const renderUsers = () => {
   const elements = getUserElements();
 
-  elements.forEach(({ el, username }) => el.setAttribute('data-u2n-username', username))
-}
+  elements.forEach(({ el, username }) => el.setAttribute('data-u2n-username', username));
+};
 
 
     renderUsers();
+  } catch (error) {
+    userScriptLogger({
+      isError: true, isCritical: true, message: 'initU2N() failed', error,
+    });
 
-    } catch (error) {
-        userScriptLogger({
-            isError: true, isCritical: true, message: 'initU2N() failed', error,
-        });
-
-        throw error;
-    }
+    throw error;
+  }
 };
 
 domReady(initU2N);
